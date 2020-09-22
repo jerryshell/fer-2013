5512ab63b4e3038f3282e6f89777f363488ba9b4
Jerry <calljerryli@outlook.com>
2020-09-22 14:08:29 +0800

    refactor get_batch_pixel_list_and_label() -> get_batch_value_and_label()

--- data_helper.py
+++ data_helper.py
@@ -23,37 +23,37 @@ class DataHelper:
         self.test_data_count = data_loader.test_data_count
 
     # data_generator 中读取 batch_size 个数据
-    def get_batch_pixel_list_and_label(self, data_generator: iter, batch_size: int):
+    def get_batch_value_and_label(self, data_generator: iter, batch_size: int):
         # 初始化返回结果
         batch_label = np.zeros(shape=(batch_size,))
-        batch_pixel_list = np.zeros(shape=(batch_size, 48, 48))
+        batch_value = np.zeros(shape=(batch_size, 48, 48))
         # 根据 batch_size 填充返回结果
         for batch_index in range(batch_size):
             # 从 data_generator 中读取下一个数据
             label, pixel_raw_data = next(data_generator)
             # pixel_raw_data2pixel_list
             pixel_list = self.pixel_raw_data2pixel_list(pixel_raw_data)
-            # pixel_list_reshape
+            # 数据重整 (48 * 48,) -> (48, 48)
             pixel_list_reshape = self.pixel_list_reshape(pixel_list)
             # 填充返回结果
             batch_label[batch_index] = label
-            batch_pixel_list[batch_index] = pixel_list_reshape
-        return batch_pixel_list, batch_label
+            batch_value[batch_index] = pixel_list_reshape
+        return batch_value, batch_label
 
     # 训练数据生成器
     def train_data_generator(self, batch_size: int):
         while True:
-            yield self.get_batch_pixel_list_and_label(self.raw_train_data_generator, batch_size)
+            yield self.get_batch_value_and_label(self.raw_train_data_generator, batch_size)
 
     # 验证数据生成器
     def validation_data_generator(self, batch_size: int):
         while True:
-            yield self.get_batch_pixel_list_and_label(self.raw_validation_data_generator, batch_size)
+            yield self.get_batch_value_and_label(self.raw_validation_data_generator, batch_size)
 
     # 测试数据生成器
     def test_data_generator(self, batch_size: int):
         while True:
-            yield self.get_batch_pixel_list_and_label(self.raw_test_data_generator, batch_size)
+            yield self.get_batch_value_and_label(self.raw_test_data_generator, batch_size)
 
     def pixel_raw_data2pixel_list(self, pixel_raw_data: str):
         return [int(item) for item in pixel_raw_data.split(' ')]
@@ -72,12 +72,15 @@ if __name__ == '__main__':
 
         label = data[0]
         pixel_raw_data = data[1]
+        print('---')
         print('pixel_raw_data', pixel_raw_data)
 
         pixel_list = data_helper.pixel_raw_data2pixel_list(pixel_raw_data)
+        print('---')
         print('pixel_list', pixel_list)
 
         pixel_list_reshape = data_helper.pixel_list_reshape(pixel_list)
+        print('---')
         print('pixel_list_reshape', pixel_list_reshape)
 
         if index == 5:
