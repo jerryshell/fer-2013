ead59a8bbc50ac79b0242a534feb59ecb84de84d
Jerry <calljerryli@outlook.com>
2020-09-27 10:28:46 +0800

    feat create_xception()

--- model_creator.py
+++ model_creator.py
@@ -1,10 +1,71 @@
 from tensorflow import keras
+from tensorflow.keras import layers
 
 data_augmentation = keras.Sequential([
     keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
     keras.layers.experimental.preprocessing.RandomRotation(0.1),
     keras.layers.experimental.preprocessing.RandomZoom(0.1),
 ])
+
+
+def create_xception(input_shape=(48, 48, 1), num_classes=7):
+    inputs = keras.Input(shape=input_shape)
+
+    # Image augmentation block
+    x = data_augmentation(inputs)
+
+    # Entry block
+    x = layers.experimental.preprocessing.Rescaling(1.0 / 255)(x)
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.Conv2D(64, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+
+    model = keras.Model(inputs, outputs)
+    model.compile(
+        optimizer=keras.optimizers.Adam(),
+        loss=keras.losses.SparseCategoricalCrossentropy(),
+        metrics=['acc'],
+    )
+    return model
 
 
 def create_resnet():
@@ -249,6 +310,11 @@ def create_model_64():
     return model
 
 
+def test_xception():
+    model = create_xception()
+    model.summary()
+
+
 def test_resnet():
     model = create_resnet()
     model.summary()
@@ -265,6 +331,7 @@ def test_model_66():
 
 
 if __name__ == '__main__':
+    test_xception()
     test_resnet()
     test_model_64()
     test_model_66()
--- model_train.py
+++ model_train.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
     data_helper = DataHelper()
 
     # 重新训练一个新模型
-    model = model_creator.create_resnet()
+    model = model_creator.create_xception()
     train(
         data_helper=data_helper,
         model=model,
