1dd0a8a4cd2196681ee6dfe28e4df1c611d8c8fc
Jerry <calljerryli@outlook.com>
2020-11-10 13:59:08 +0800

    chore update docker scripts

--- tf_serving_docker.sh
+++ tf_serving_docker.sh
@@ -1,1 +1,2 @@
-docker run --rm -p 8501:8501 -v "$(pwd)/model_tf:/models/fer/0" -e MODEL_NAME=fer tensorflow/serving
+#!/usr/bin/env bash
+docker run --rm -it -p 8501:8501 -v "$(pwd)/model_tf:/models/fer/0" -e MODEL_NAME=fer tensorflow/serving
