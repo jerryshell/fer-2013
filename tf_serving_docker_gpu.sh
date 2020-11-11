7b9c159983a235bd5fa8b8cd63c97502a488ada5
Jerry <calljerryli@outlook.com>
2020-11-11 14:45:01 +0800

    chore update tf_serving_docker_gpu.sh

--- tf_serving_docker_gpu.sh
+++ tf_serving_docker_gpu.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --name tf_serving_fer --gpus all -d -p 8585:8501 -v "$(pwd)/model_tf:/models/fer/0" -e MODEL_NAME=fer tensorflow/serving:latest-gpu
+docker run --name tf_serving_fer --gpus all -d -p 8501:8501 -v "$(pwd)/model_tf:/models/fer/0" -e MODEL_NAME=fer tensorflow/serving:latest-gpu
